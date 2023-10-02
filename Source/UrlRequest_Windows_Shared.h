d7e84ae82138a80171aaffa39ec50d0d7340e529
Andy Fillebrown <afillebrown@microsoft.com>
2023-10-02 19:57:46 -0400

    .

--- Source/UrlRequest_Windows_Shared.h
+++ Source/UrlRequest_Windows_Shared.h
@@ -55,6 +55,89 @@ namespace UrlLib
         gsl::span<const std::byte> ResponseBuffer() const;
 
     private:
+        arcana::task<void, std::exception_ptr> LoadHttpAsync()
+        {
+            Web::Http::HttpRequestMessage requestMessage;
+            requestMessage.RequestUri(m_uri);
+            requestMessage.Method(ConvertHttpMethod(m_method));
+
+            std::string contentType;
+
+            for (auto request : m_requestHeaders)
+            {
+                // content type needs to be set separately
+                if (request.first == "Content-Type")
+                {
+                    contentType = request.second;
+                }
+                else
+                {
+                    requestMessage.Headers().Append(winrt::to_hstring(request.first), winrt::to_hstring(request.second));
+                }
+            }
+
+            m_requestHeaders.clear();
+
+            // check the method
+            if (m_method == UrlMethod::Post)
+            {
+                // if post, set the content type
+                requestMessage.Content(Web::Http::HttpStringContent(
+                    winrt::to_hstring(m_requestBody),
+                    winrt::Windows::Storage::Streams::UnicodeEncoding::Utf8,
+                    winrt::to_hstring(contentType))
+                );
+            }
+
+            Web::Http::HttpClient client;
+            return arcana::create_task<std::exception_ptr>(client.SendRequestAsync(requestMessage))
+                .then(arcana::inline_scheduler, m_cancellationSource, [this](Web::Http::HttpResponseMessage responseMessage)
+                {
+                    m_statusCode = static_cast<UrlStatusCode>(responseMessage.StatusCode());
+                    if (!responseMessage.IsSuccessStatusCode())
+                    {
+                        return arcana::task_from_result<std::exception_ptr>();
+                    }
+
+                    m_responseUrl = winrt::to_string(responseMessage.RequestMessage().RequestUri().RawUri());
+
+                    for (auto&& iter : responseMessage.Headers())
+                    {
+                        m_headers.insert(std::make_pair(winrt::to_string(iter.Key()), winrt::to_string(iter.Value())));
+                    }
+                    // process the content type response header
+                    std::string contentTypeValue = winrt::to_string(responseMessage.Content().Headers().ContentType().ToString());
+                    std::string contentTypeKey = "content-type";
+                    m_headers.insert(std::make_pair(contentTypeKey, contentTypeValue));
+
+                    switch (m_responseType)
+                    {
+                        case UrlResponseType::String:
+                        {
+                            return arcana::create_task<std::exception_ptr>(responseMessage.Content().ReadAsStringAsync())
+                                .then(arcana::inline_scheduler, m_cancellationSource, [this](winrt::hstring string)
+                                {
+                                    m_responseString = winrt::to_string(string);
+                                    m_statusCode = UrlStatusCode::Ok;
+                                });
+                        }
+                        case UrlResponseType::Buffer:
+                        {
+                            return arcana::create_task<std::exception_ptr>(responseMessage.Content().ReadAsBufferAsync())
+                                .then(arcana::inline_scheduler, m_cancellationSource, [this](Storage::Streams::IBuffer buffer)
+                                {
+                                    m_responseBuffer = std::move(buffer);
+                                    m_statusCode = UrlStatusCode::Ok;
+                                });
+                        }
+                        default:
+                        {
+                            throw std::runtime_error{"Invalid response type"};
+                        }
+                    }
+                });
+        }
+
 #if !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
         arcana::task<void, std::exception_ptr> LoadFileAsync(Storage::StorageFile file);
 #else
